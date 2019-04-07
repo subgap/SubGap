b584f6bd55a6658918c9be064279608aef66c1af
FireMasterK <kavin@rape.lol>
2019-04-07 22:50:19 +0530

    Switching to properties for constants

--- src/main/java/me/kavin/subgap/consts/Constants.java
+++ src/main/java/me/kavin/subgap/consts/Constants.java
@@ -1,14 +1,31 @@
 package me.kavin.subgap.consts;
 
 import java.awt.Color;
+import java.io.FileReader;
+import java.io.IOException;
 import java.text.DecimalFormat;
+import java.util.Properties;
 
 public class Constants {
-	public static final String BOT_TOKEN = System.getenv().get("BOT_TOKEN");
 
-	public static final String GOOGLE_API_KEY = System.getenv().get("GOOGLE_API_KEY");
+	static {
+		Properties prop = new Properties();
+		try {
+			prop.load(new FileReader("config.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-	public static final String FIREBASE_CREDENTIALS_URL = System.getenv().get("FIREBASE_CREDENTIALS_URL");
+		BOT_TOKEN = prop.getProperty("BOT_TOKEN");
+		GOOGLE_API_KEY = prop.getProperty("GOOGLE_API_KEY");
+		FIREBASE_CREDENTIALS_URL = prop.getProperty("FIREBASE_CREDENTIALS_URL");
+	}
+
+	public static final String BOT_TOKEN;
+
+	public static final String GOOGLE_API_KEY;
+
+	public static final String FIREBASE_CREDENTIALS_URL;
 
 	public static final Color NO_COLOR_EMBED = new Color(54, 57, 63);
 
